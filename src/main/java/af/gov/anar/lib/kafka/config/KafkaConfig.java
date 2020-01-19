c52b18e3b3ef5187f4ea04921d9793814df0e68b
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-19 14:25:35 +0430

    refactoring

--- src/main/java/af/gov/anar/lib/kafka/config/KafkaConfig.java
+++ src/main/java/af/gov/anar/lib/kafka/config/KafkaConfig.java
@@ -1,10 +1,18 @@
 package af.gov.anar.lib.kafka.config;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
+@EnableKafka
+@ComponentScan(basePackages = {"af.*"})
 public class KafkaConfig {
 
     @Bean

462c798c162f0a378850c613a98ca406c3cde75f
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-19 12:31:24 +0430

    consumer config

--- src/main/java/af/gov/anar/lib/kafka/consumer/AnarConsumer.java
+++ src/main/java/af/gov/anar/lib/kafka/consumer/AnarConsumer.java
@@ -1,9 +1,15 @@
 package af.gov.anar.lib.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnarConsumer<T> {
 
 
+    @KafkaListener(topics = this.to )
+    public T receive(T obj) {
+        return obj;
+    }
 }
